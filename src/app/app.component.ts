1b01feb36abcec92e396d14d0bdd91b5a8487584
Adjaratou09 <adjara.niho.diarra@gmail.com>
2025-05-19 00:24:19 +0200

    les component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,AdminComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path :'',
+        component: HomepageComponent
+    },
+    {
+        path: '',
+        component:HeaderComponent
+    },
+    {path:'Admin',
+        component:AdminComponent
+
+    },
+    {path:'Connexion',
+        component:LoginComponent
+
+    },
+
+];
