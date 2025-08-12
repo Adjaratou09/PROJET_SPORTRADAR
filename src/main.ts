df4fc7564c3058191436b53ddbd05cb99760949c
Adjaratou09 <adjara.niho.diarra@gmail.com>
2025-08-13 00:16:10 +0200

    resolution problème page connexion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/*import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
+*/
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment'; // ✅ chemin correct
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)), // ✅ options fournies
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
+});
 
