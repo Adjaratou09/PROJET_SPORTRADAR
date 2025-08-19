20824a46190ade049aa72b4601a17a6768f8b847
Adjaratou09 <adjara.niho.diarra@gmail.com>
2025-08-20 01:55:20 +0200

    mise en forme

--- src/app/abonnement/abonnement.component.ts
+++ src/app/abonnement/abonnement.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-abonnement',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './abonnement.component.html',
   styleUrl: './abonnement.component.scss'
 })
